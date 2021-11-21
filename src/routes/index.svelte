d3ac93f353c63dfe3516cd58395eb70e53c25048
Jason Gutierrez <jasong84@gmail.com>
2021-11-20 21:56:54 -0600

    Save the proof of concept for audio recording

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte'
+
+	let media = []
+	let mediaRecorder = null;
+	onMount(async () => {
+		const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+		
+		mediaRecorder = new MediaRecorder(stream);
+		mediaRecorder.ondataavailable = (e) => media.push(e.data)
+		mediaRecorder.onstop = function(){
+			const audio = document.querySelector('audio');
+			const blob = new Blob( media, {'type' : 'audio/ogg; codecs=opus' });
+			media = [];
+			audio.src = window.URL.createObjectURL(blob);
+		}
+		
+	})
+	
+	function startRecording(){
+		mediaRecorder.start();
+	}
+
+	function stopRecording(){
+		mediaRecorder.stop();
+	}
+
+</script>
+
+<section>
+	<h2>Sound Board</h2>
+	<audio controls/>
+	<button on:click={startRecording}>Record</button>
+	<button on:click={stopRecording}>Stop</button>
+</section>
+
+<style>
+	section{
+		display: flex;
+		flex-flow: column;
+		width: 300px;
+	}
+</style>
